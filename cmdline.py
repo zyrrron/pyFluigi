217791b2b6af75e4c2d9471b6069abebb69cc6ee
Radhakrishna Sanka <rkrishnasanka@gmail.com>
2020-04-13 16:43:38 -0400

    Updated the MINT parser for basic level of MINT parsing

---
+++ cmdline.py
@@ -0,0 +1,56 @@
+import sys
+import os
+from pathlib import Path
+import time
+from antlr4 import *
+import argparse
+import parameters
+
+from pyMINT.antlr.mintLexer import mintLexer
+from pyMINT.antlr.mintParser import mintParser
+from pyMINT.mintcompiler import MINTCompiler
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        'input', help="This is the file thats used as the input ")
+    parser.add_argument('--outpath', type=str, default="out/",
+                        help="This is the output directory")
+
+    args = parser.parse_args()
+    print("output dir:", args.outpath)
+    print(args.input)
+
+    extension = Path(args.input).suffix
+    if extension != '.mint' and extension != '.uf' :
+        print("Unrecognized file Extension")
+        exit()
+
+    abspath = os.path.abspath(args.outpath)
+    parameters.OUTPUT_DIR = abspath
+
+    if os.path.isdir(abspath) is not True:
+        print("Creating the output directory:")
+        path = Path(parameters.OUTPUT_DIR)
+        path.mkdir(parents=True)
+
+    finput = FileStream(args.input)
+
+    lexer = mintLexer(finput)
+
+    stream = CommonTokenStream(lexer)
+
+    parser = mintParser(stream)
+
+    tree = parser.netlist()
+
+    walker = ParseTreeWalker()
+
+    listener = MINTCompiler()
+
+    walker.walk(listener, tree)
+
+if __name__ == "__main__":
+    main()
