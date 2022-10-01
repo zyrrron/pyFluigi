2609a8cda279cce0bfef2850dcb62f4ebc0c0e9e
Radhakrishna Sanka <rkrishnasanka@gmail.com>
2022-10-01 16:19:47 +0000

    Chmodded the scripts and also modified the conversion script

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -2,23 +2,23 @@
 
 echo "Converting the MINT Files"
 
-for f in ~/CIDAR/MINT-TestCases/chthesis/*.mint;
+for f in /root/CIDAR/Microfluidics-Benchmarks/MINT-TestCases/chthesis/*.mint;
 
 do
     echo "Runnign File $f";
-    fluigi convert-to-parchmint $f --assign-terminals --generate-graph-view --outpath ~/Desktop/MINT-to-json/chthesis
+    fluigi convert-to-parchmint $f --assign-terminals --generate-graph-view --outpath ~/MINT-to-json/chthesis
 done
 
-for f in ~/CIDAR/MINT-TestCases/dropx_ref/*.mint;
+for f in /root/CIDAR/Microfluidics-Benchmarks/MINT-TestCases/dropx_ref/*.mint;
 
 do
     echo "Runnign File $f";
-    fluigi convert-to-parchmint $f --assign-terminals --generate-graph-view --outpath ~/Desktop/MINT-to-json/dropx_ref
+    fluigi convert-to-parchmint $f --assign-terminals --generate-graph-view --outpath ~/MINT-to-json/dropx_ref
 done
 
-for f in ~/CIDAR/MINT-TestCases/grid/*.mint;
+for f in /root/CIDAR/Microfluidics-Benchmarks/MINT-TestCases/grid/*.mint;
 
 do
     echo "Runnign File $f";
-    fluigi convert-to-parchmint $f --assign-terminals --generate-graph-view --outpath ~/Desktop/MINT-to-json/grid
+    fluigi convert-to-parchmint $f --assign-terminals --generate-graph-view --outpath ~/MINT-to-json/grid
 done
