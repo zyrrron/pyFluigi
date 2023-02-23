340b16fc0c0c8ccf925d48231bae59e92db45417
Radhakrishna Sanka <rkrishnasanka@gmail.com>
2023-02-23 21:44:31 +0000

    Fixed error where the port data was passed to incorrect argument.

--- fluigi/primitives.py
+++ fluigi/primitives.py
@@ -73,9 +73,9 @@ def get_terminals(mint: str, params):
         python_object = []
 
         for terminal in terminals:
-            componentport = Port(terminal)
+            componentport = Port(json_data=terminal)
             python_object.append(componentport)
-            # print(componentport)
+            print(componentport)
 
         return python_object
 
